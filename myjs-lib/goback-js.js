166efb7987d3ac7e85cc780fa942b80a1b715cf9
AGirlisnoone <DenisseDato@users.noreply.github.com>
2017-09-21 07:12:02 +0800

    test test tes

---
+++ myjs-lib/goback-js.js
@@ -0,0 +1,5 @@
+function goBack() {
+	document.write("<form action='index.html'>");
+	document.write("	<input type='submit' style='margin-left: 50%' value='GO BACK!'>");
+	document.write("</form>");
+}
